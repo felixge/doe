8a057f02def43c77d2d09825d958eb8e4b3a4ca2
Felix Geisendörfer <felix.geisendoerfer@datadoghq.com>
2026-09-23 17:55:57 +0200

    refactor(cmd): name results variable consistently

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -71,11 +71,11 @@ func experimentCommand(ctx context.Context, env *cli.Env, args []string) int {
 		}
 		return env.Fail(err)
 	}
-	records, err := results.New(filepath.Join(dir, "results"))
+	results, err := results.New(filepath.Join(dir, "results"))
 	if err != nil {
 		return env.Fail(err)
 	}
-	if err := records.AppendExperiment(experiment); err != nil {
+	if err := results.AppendExperiment(experiment); err != nil {
 		return env.Fail(err)
 	}
 	return 0
